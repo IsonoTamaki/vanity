7397b1f807bba9c571191276fb05c1ed18bbd02f
IsonoTamaki <69899618+IsonoTamaki@users.noreply.github.com>
2021-08-17 14:25:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,32 @@
 		$nav.removeClass( open );
 	});
   } )(jQuery);
+
+var bar = new ProgressBar.Line(splash_count, {
+	easing: 'easeInOut',
+	duration: 1000,
+	strokeWidth: 0.2,
+	color: '#e5e5e5',
+	trailWidth: 0.2,
+	trailColor: '#555',
+	text: {
+		style: {
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-10px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+bar.animate(1.0, function () {
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
